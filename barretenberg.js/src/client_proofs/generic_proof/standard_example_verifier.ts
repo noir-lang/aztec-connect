dd1bf7fa1318e46b55432cc946878d7d6059fbc4
kevaundray <kevtheappdev@gmail.com>
2022-09-20 15:59:00 +0000

    add generic proofs

---
+++ barretenberg.js/src/client_proofs/generic_proof/standard_example_verifier.ts
@@ -0,0 +1,20 @@
+import { BarretenbergWorker } from '../../wasm/worker';
+import { SinglePippenger } from '../../pippenger';
+
+export class StandardExampleVerifier {
+  private worker!: BarretenbergWorker;
+
+  public async computeKey(pippenger: SinglePippenger, g2Data: Uint8Array) {
+    this.worker = pippenger.getWorker();
+    await this.worker.transferToHeap(g2Data, 0);
+    await this.worker.call('standard_example__init_verification_key', pippenger.getPointer(), 0);
+  }
+
+  public async verifyProof(proof: Buffer) {
+    const proofPtr = await this.worker.call('bbmalloc', proof.length);
+    await this.worker.transferToHeap(proof, proofPtr);
+    const verified = (await this.worker.call('standard_example__verify_proof', proofPtr, proof.length)) ? true : false;
+    await this.worker.call('bbfree', proofPtr);
+    return verified;
+  }
+}

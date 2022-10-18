8155180456da48fc786ff084a659afae1a4d92c0
Globallager <72797635+Globallager@users.noreply.github.com>
2022-10-18 21:58:14 +0800

    Rename `c_bind.hpp` → `c_bind.h`
    Follow-up patch to changes in commit [d92b0453](https://github.com/noir-lang/aztec-connect/commit/d92b04535d3e4af5ad5a622d5483266844c17e08).

--- barretenberg/src/aztec/bb/bb.hpp
+++ barretenberg/src/aztec/bb/bb.hpp
@@ -5,4 +5,4 @@
 #include <ecc/curves/grumpkin/c_bind.hpp>
 #include <ecc/curves/bn254/scalar_multiplication/c_bind.hpp>
 #include <crypto/schnorr/c_bind.hpp>
-#include <dsl/standard_format/c_bind.hpp>
+#include <dsl/standard_format/c_bind.h>

d47255e6ba88c3a2ae6c6ab56a910893fbc02247
Koby <koby@aztecprotocol.com>
2023-01-26 17:22:12 +0100

    Adding wasm target (tbr)

---
+++ barretenberg/barretenberg.nix
@@ -0,0 +1,22 @@
+{ stdenv, cmake, openmp, leveldb }:
+stdenv.mkDerivation rec {
+  pname = "libbarretenberg";
+  version = "0.1.0";
+
+  src = ./.;
+
+  # dontUseCmakeConfigure as per https://nixos.org/manual/nixpkgs/stable/#cmake
+  dontUseCmakeConfigure = true;
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ openmp leveldb ];
+  NIX_CFLAGS_COMPILE =
+    if (stdenv.isDarwin) then [ " -fno-aligned-allocation" ] else null;
+  buildPhase = ''
+    cmake -DCMAKE_BUILD_TYPE=RelWithAssert -DNIX_VENDORED_LIBS=ON -DTESTING=OFF -DBENCHMARKS=OFF .
+    cmake --build . --parallel
+  '';
+  installPhase = ''
+    mkdir -p $out/lib
+    find src -name \*.a -exec cp {} $out/lib \;
+  '';
+}

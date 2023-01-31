98060e0ef2bce31b16904dda1a3e47572be24f69
Blaine Bublitz <blaine@aztecprotocol.com>
2023-01-31 15:53:46 -0700

    some issue claimed we needed pkgsLLVM

--- flake.nix
+++ flake.nix
@@ -22,9 +22,9 @@
           (
             [ ] ++ optional (system == "x86_64-linux") {
               name = "cross-aarch64-multiplatform";
-              value = pkgs.pkgsCross.aarch64-multiplatform-musl.callPackage ./barretenberg/barretenberg.nix {
-                stdenv = pkgs.pkgsCross.aarch64-multiplatform-musl.llvmPackages_14.stdenv;
-                llvmPackages = pkgs.pkgsCross.aarch64-multiplatform-musl.llvmPackages_14;
+              value = with pkgs.pkgsCross.aarch64-multiplatform.pkgsLLVM; callPackage ./barretenberg/barretenberg.nix {
+                stdenv = llvmPackages_11.stdenv;
+                llvmPackages = llvmPackages_11;
               };
             } ++ optional (system == "x86_64-darwin") {
               name = "cross-aarch64-darwin";

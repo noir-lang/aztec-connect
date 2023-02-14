37794842b8f26784dd1086bdab76bbfeb6d970be
vezenovm <mvezenov@gmail.com>
2023-02-14 11:37:00 -0500

    remove dbg

--- barretenberg_wrapper/build.rs
+++ barretenberg_wrapper/build.rs
@@ -204,7 +204,6 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    dbg!(out_path.clone());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings");
